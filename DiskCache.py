59f0cfafed477479d52e89eb47c3caaaf4c84331
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2021-08-07 16:41:01 +0430

    Add torch save

--- DiskCache.py
+++ DiskCache.py
@@ -4,44 +4,65 @@ from pathlib import Path
 import json
 import os
 from shutil import rmtree
-import bz2
 import pickle
 import _pickle as cPickle
+import gc
+import marshal
+import shelve
+import torch
 
 
 class DiskCache(MutableMapping, ABC):
-    def __init__(self, folder_name, save_as="pickle"):
+    def __init__(self, folder_name, save_as="torch"):
         self.folder_name = folder_name
         self.save_as = save_as
         self.clear()
+        self.shelve_cache = shelve.open("cache_tmp/shelve.shelve")
 
     def make_path(self, key):
         return self.folder_name + "/" + str(key)
 
     def __setitem__(self, key, value):
+        gc.disable()
         self.keys[key] = True
         file_path = self.make_path(key)
         if self.save_as == "json":
             with open(f"{file_path}.json", "w") as f:
                 json.dump(value, f)
-        else:
+        elif self.save_as == "pickle":
             with open(f"{file_path}.pickle", "wb") as f:
-                cPickle.dump(value, f)
+                cPickle.dump(value, f, protocol=pickle.HIGHEST_PROTOCOL)
+        elif self.save_as == "marshal":
+            with open(f"{file_path}.marshal", "wb") as f:
+                marshal.dump(value, f)
+        elif self.save_as == "shelve":
+            self.shelve_cache[str(key)] = value
+        elif self.save_as == "torch":
+            torch.save(value, f"{file_path}.pt")
+
+        gc.enable()
         # else:
         #     with bz2.BZ2File(f"{file_path}.pbz2", 'w') as f:
         #         cPickle.dump(value, f)
 
     def __getitem__(self, key):
+        gc.disable()
         file_path = self.make_path(key)
         if self.save_as == "json":
             with open(f"{file_path}.json", "r") as f:
-                return json.load(f)
-        else:
+                data = json.load(f)
+        elif self.save_as == "pickle":
             with open(f"{file_path}.pickle", "rb") as f:
-                return cPickle.load(f)
-        # else:
-        #     data = bz2.BZ2File(f"{file_path}.pbz2", "rb")
-        #     return cPickle.load(data)
+                data = cPickle.load(f)
+        elif self.save_as == "marshal":
+            with open(f"{file_path}.marshal", "rb") as f:
+                data = marshal.load(f)
+        elif self.save_as == "shelve":
+            data = self.shelve_cache[str(key)]
+        elif self.save_as == "torch":
+            data = torch.load(f"{file_path}.pt")
+        gc.enable()
+        return data
 
     def __contains__(self, key):
         return self.keys.__contains__(key)
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 from DiskCache import DiskCache
 from timeit import timeit
 from tqdm.auto import tqdm
+import torch
 
-disk_cache = DiskCache("cache_tmp", save_as="pickle")
-for j in tqdm(range(15)):
-    disk_cache[j*32] = ["b", "a", {"a": "c"}, [i**2+0.1654 for i in range(768*2*13*32)]]
-for j in tqdm(range(15)):
-    a = disk_cache[j*32]
+disk_cache = DiskCache("cache_tmp", save_as="torch")
+for j in tqdm(range(10)):
+    disk_cache[j*32] = ["b", "a", {"a": "c"}, torch.tensor([i**2*0.1651875423 for i in range(768*4*13*32)])]
+for _ in range(10):
+    for j in tqdm(range(10)):
+        if j*32 in disk_cache:
+            a = disk_cache[j*32]
 print("0" in disk_cache)
 print("b" in disk_cache)
